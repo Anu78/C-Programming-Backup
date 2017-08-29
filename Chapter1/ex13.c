45515bd149e751f9e1babc23b070d0e745a25ac1
Anuraag <anuraag.warudkar@gmail.com>
2017-08-29 16:36:05 -0400

    Finished ex13.

--- Chapter1/ex13.c
+++ Chapter1/ex13.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 #define IN 1
 #define OUT 0
+#define MAX_LENGTH 15
 int main()
 {
     int array[50] = {};
-    int c,char_count=0,i,arrayPosition=0;
+    int c,char_count=0,i,arrayPosition=0,j;
 
     while((c=getchar()) != EOF) {
         ++char_count;
@@ -19,10 +20,14 @@ int main()
         printf("%d\n",array[j]);
     }
 //Print Histogram
-    printf("\n");
-    for(i=1; i<15; ++i) {
-        printf("%d\n",i);
+    printf(" length | graph\n");
+    for (i = 0; i <= MAX_LENGTH; ++i) {
+        if (i != MAX_LENGTH)
+            printf("     %2d | ", i + 1);
+        else
+            printf("    >%2d | ", MAX_LENGTH);
+        for (j = 0; j < array[i]; ++j)
+            putchar('+');
+        putchar('\n');
     }
-    printf("%s\n","15+");
-    return 0;
 }
