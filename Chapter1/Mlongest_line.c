141e318961482732d343393c17036c9805acb42b
Anuraag Warudkar <anuraagw@Anuraags-MacBook-Air.local>
2018-08-06 18:14:10 -0400

    Longest Line exercise and exercise 16 finished

---
+++ Chapter1/Mlongest_line.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define MAXLINE 1000
+
+void copy1(char to[], char from[]);
+int getline1(char line[], int maxline);
+
+main()
+{
+    int len;
+    int max;
+    char line[MAXLINE];
+    char longest[MAXLINE];
+
+    max=0;
+    while((len=getline1(line,MAXLINE)) > 0)
+        if (len> max) {
+            max = len;
+            copy1(longest, line);
+        }
+    if (max> 0)
+        printf("%s", longest);
+    return 0;
+}
+
+int getline1(char s[], int lim)
+{
+    int c,i;
+
+    for(i=0; i<lim-1 && (c=getchar()) !=EOF && c !='\n'; ++i)
+        s[i] = c;
+    if(c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return i;
+}
+
+void copy1(char to[],char from[])
+{
+    int i;
+    i = 0;
+    while ((to[i] = from[i]) != 0)
+        ++i;
+}
