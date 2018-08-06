a64487c029d8bf106da5509c108e4f3302b339c7
Anuraag Warudkar <anuraagw@Anuraags-MacBook-Air.local>
2018-08-06 16:28:42 -0400

    Finished exercise 15

--- Chapter1/ex15.c
+++ Chapter1/ex15.c
@@ -1,7 +1,22 @@
+//rewrite the function from 1.2 to use a function to convert the temperatures
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
+#define YES 0
+
+int convert(int number, int choice)
 {
-     /* code */
-     return 0;
+    float final_value = 2.01;
+    if(choice == 0) {
+        final_value = (number - 32)*(5.0/9.0);
+        printf("%4.2f",final_value);
+    }
+    else {
+        final_value = number*1.8 + 32;
+    }
+    return final_value;
+}
+
+int main()
+{
+    printf("%d\n",convert(40,0));
 }
