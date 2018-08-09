087930716614176f55060dda62c02b9b83d03ac4
Anuraag Warudkar <anuraagw@Anuraags-MacBook-Air.local>
2018-08-09 19:29:32 -0400

    Exercise 17 finished, exercise 18 started

--- Chapter1/ex17.c
+++ Chapter1/ex17.c
@@ -1,12 +1,39 @@
 //Print all input lines longer than 80 characters
 #include <stdio.h>
 
+#define LARGESTLINE 1000
+#define TARGETSIZE 80
+
+
+int getline2(char line[], int lim);
+
+
 int main(int argc, char const *argv[])
 {
-     int c;
+    int len;
+    char line[LARGESTLINE];
 
-     while((c = getchar()) != '\n'){
-          
-     }
-     return 0;
+    while(((len = getline2(line,LARGESTLINE)) > 0)) {
+        if(len > TARGETSIZE) {
+            printf("%s",line);
+        }
+    }
+    return 0;
+}
+
+int getline2(char line[], int limit) {
+    int c,i;
+
+    for(i=0; i < limit - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        line[i]=c;
+    }
+    if(c == '\n')
+    {
+        line[i] = c;
+        ++i;
+    }
+
+    line[i] = '\0';
+
+    return i;
 }
