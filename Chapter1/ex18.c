7dde447e7787b93c486b05e707e16b7bccdfe2e5
Anuraag Warudkar <anuraagw@Anuraags-MacBook-Air.local>
2018-08-09 19:31:55 -0400

    what

---
+++ Chapter1/ex18.c
@@ -0,0 +1,11 @@
+//Write a program to remove trailing blanks and tabs from each line of input, and to delete entirely blank lines
+
+#include <stdio.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+     
+     return 0;
+}
